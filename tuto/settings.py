4127c0fed6ca6703a0e0f82c3eab563b5131f3e8
unknown <bahamiled@live.fr>
2025-03-07 01:10:40 +0100

    'http'

--- tuto/settings.py
+++ tuto/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-c8)$8)l8z%od%7n5p%3-ey)=sd+37ro-unhmjtc+smcpge9=g^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['djangoproject-47ri.onrender.com','localhost',
+ALLOWED_HOSTS = ['localhost',
     '127.0.0.1',]
 
 
@@ -135,7 +135,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True  # Redirect HTTP → HTTPS
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#SECURE_SSL_REDIRECT = True  # Redirect HTTP → HTTPS
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
